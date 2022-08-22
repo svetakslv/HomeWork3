9654218e087c5da53467e7c31d1dd2eb8f9502e1
Светка <sveta.chakrygina@mail.ru>
2022-08-22 23:28:29 +0300

    Задачи №1,№2,№3,№4,№5,№6

--- src/Main.java
+++ src/Main.java
@@ -37,73 +37,91 @@ public class Main {
         if (year % 4 == 0) {
             System.out.println("Это високосный год");
         } else {
-            if (year / 4 != 0) {
-                System.out.println("Это не високосный год");
-            }
+
+            System.out.println("Это не високосный год");
+        }
 
 
-            //Задача №4
-            int deliveryDistance = 95;
-            if (deliveryDistance < 20) {
-                System.out.println("Потребуется 1 день");
+        //Задача №4
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется 1 день");
+        } else {
+            if (deliveryDistance >= 20 && deliveryDistance <= 60) {
+                System.out.println("Потребуется 2 дня");
             } else {
-                if (deliveryDistance >= 20 && deliveryDistance <= 60) {
-                    System.out.println("Потребуется 2 дня");
-                } else {
-                    if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-                        System.out.println("Потребуется 3 дня");
-                    }
+                if (deliveryDistance >= 60 && deliveryDistance <= 100) {
+                    System.out.println("Потребуется 3 дня");
                 }
             }
-            //Задача №5
-            int monthNumber = 13;
+        }
+        //Задача №5
+        int monthNumber = 13;
 
-            switch (monthNumber) {
-                case 1:
-                    System.out.println("Зимний месяц");
-                    break;
-                case 2:
-                    System.out.println("Зимний месяц");
-                    break;
-                case 12:
-                    System.out.println("Зимний месяц");
-                    break;
-                case 3:
-                    System.out.println("Весенний месяц");
-                    break;
-                case 4:
-                    System.out.println("Весенний месяц");
-                    break;
-                case 5:
-                    System.out.println("Весенний месяц");
-                    break;
-                case 6:
-                    System.out.println("Летний месяц");
-                    break;
-                case 7:
-                    System.out.println("Летний месяц");
-                    break;
-                case 8:
-                    System.out.println("Летний месяц");
-                    break;
-                case 9:
-                    System.out.println("Осенний месяц");
-                    break;
-                case 10:
-                    System.out.println("Осенний месяц");
-                    break;
-                case 11:
-                    System.out.println("Осенний месяц");
-                    break;
-                case 13:
-                    System.out.println("Такого месяца не существует");
-                    break;
+        switch (monthNumber) {
+            case 1:
+                System.out.println("Зимний месяц");
+                break;
+            case 2:
+                System.out.println("Зимний месяц");
+                break;
+            case 12:
+                System.out.println("Зимний месяц");
+                break;
+            case 3:
+                System.out.println("Весенний месяц");
+                break;
+            case 4:
+                System.out.println("Весенний месяц");
+                break;
+            case 5:
+                System.out.println("Весенний месяц");
+                break;
+            case 6:
+                System.out.println("Летний месяц");
+                break;
+            case 7:
+                System.out.println("Летний месяц");
+                break;
+            case 8:
+                System.out.println("Летний месяц");
+                break;
+            case 9:
+                System.out.println("Осенний месяц");
+                break;
+            case 10:
+                System.out.println("Осенний месяц");
+                break;
+            case 11:
+                System.out.println("Осенний месяц");
+                break;
+            case 13:
+                System.out.println("Такого месяца не существует");
+                break;
+        }
+
+
+        //*Задача №6
+        int age = 24;
+        int salary = 58_000;
+        if (age < 23) {
+            System.out.println("Вам доступно для снятия " + salary * 2);
+        } else {
+            if (age >= 23) {
+                System.out.println("Вам доступно для снятия " + salary * 3);
+                if (salary >= 50_000 && salary < 79_000) {
+                    System.out.println("Ваш лимит увеличен до " + salary * 1.2);
+                } else {
+                    if (salary >= 80_000) {
+                        System.out.println("Ваш лимит увеличен до " + salary * 1.5);
+                    }
+                }
+
             }
         }
     }
 }
 
-            //*Задача №6
 
 
 
