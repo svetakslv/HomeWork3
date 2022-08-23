fd47796788ff8fa066af5746d7518e4e681f72eb
Светка <sveta.chakrygina@mail.ru>
2022-08-23 12:51:57 +0300

    Задачи №1,№2,№3,№4,№5,№6

--- src/Main.java
+++ src/Main.java
@@ -7,56 +7,55 @@ public class Main {
         if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
-            if (clientOS == 1) {
+            if (clientOS == 1) { // Эта строка лишняя.
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
         }
 
 
         //Задача №2
-        int OSClient = 0;
-        int clientDeviceYear = 2015;
-        if (OSClient == 0 && clientDeviceYear >= 2015) {
-            System.out.println("Приложение будет работать корректно на iOS");
-        } else {
-            if (OSClient == 1 && clientDeviceYear >= 2015) {
-                System.out.println("Приложение будет работать корректно на Android");
-            } else {
-                if (OSClient == 0 && clientDeviceYear < 2015) {
-                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                } else {
-                    if (OSClient == 1 && clientDeviceYear < 2015) {
-                        System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                    }
+        int OSClient = 1;
+        int clientDeviceYear = 2010;                                                                             //Можно упростить код:
+        if (OSClient == 0 && clientDeviceYear >= 2015) {                                                         //if (OSClient == 0) {
+            System.out.println("Приложение будет работать корректно на iOS");                                    //if (clientDeviceYear >= 2015) {
+        } else {                                                                                                 //System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            if (OSClient == 1 && clientDeviceYear >= 2015) {                                                     // } else {
+                System.out.println("Приложение будет работать корректно на Android");                            //System.out.println("Приложение будет работать корректно на iOS")
+            } else {                                                                                             // }
+                if (OSClient == 0 && clientDeviceYear < 2015) {                                                  // } else {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");            //if (clientDeviceYear < 2015) {
+                } else {                                                                                         //System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                    if (OSClient == 1 && clientDeviceYear < 2015) {                                              // } else{
+                        System.out.println("Установите облегченную версию приложения для Android по ссылке");    //System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                    }                                                                                            //
                 }
             }
         }
 
         //Задача №3
-        int year = 2022;
-        if (year % 4 == 0) {
-            System.out.println("Это високосный год");
-        } else {
-
-            System.out.println("Это не високосный год");
+        int year = 2024;                                         // Необходимо было немного дополнить задачу:
+        if (year % 4 == 0) {                                     //if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println("Это високосный год");            //System.out.println(year + " является високосным годом");
+        } else {                                                 //} else {
+            System.out.println("Это не високосный год");         //System.out.println(year + " не является високосным годом");
         }
 
 
         //Задача №4
-        int deliveryDistance = 95;
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется 1 день");
-        } else {
-            if (deliveryDistance >= 20 && deliveryDistance <= 60) {
-                System.out.println("Потребуется 2 дня");
-            } else {
-                if (deliveryDistance >= 60 && deliveryDistance <= 100) {
-                    System.out.println("Потребуется 3 дня");
-                }
-            }
+        int deliveryDistance = 95;                                       //Можно было сделать так:
+        if (deliveryDistance < 20) {                                     //int deliveryDistance = 95;
+            System.out.println("Потребуется 1 день");                    //int deliveryDays = 1;
+        } else {                                                         //if (deliveryDistance > 20) {
+            if (deliveryDistance >= 20 && deliveryDistance <= 60) {      //  deliveryDays++;
+                System.out.println("Потребуется 2 дня");                 // }
+            } else {                                                     // if (deliveryDistance > 60) {
+                if (deliveryDistance >= 60 && deliveryDistance <= 100) { // deliveryDays++;
+                    System.out.println("Потребуется 3 дня");             // }
+                }                                                        //System.out.println("Потребуется дней: " + deliveryDays);
+            }                                                            // }
         }
-        //Задача №5
-        int monthNumber = 13;
+        //Задача №5                                                    //Исправления не нужны :)
+        int monthNumber = 12;
 
         switch (monthNumber) {
             case 1:
@@ -102,8 +101,8 @@ public class Main {
 
 
         //*Задача №6
-        int age = 24;
-        int salary = 58_000;
+        int age = 46;
+        int salary = 86_000;
         if (age < 23) {
             System.out.println("Вам доступно для снятия " + salary * 2);
         } else {
